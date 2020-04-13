a074e488568b78a990ce9afb34112df6e4d5ab26
Teresa Wells <teresaserena12@gmail.com>
2020-04-12 21:31:13 -0500

    adding month view

--- src/components/App.js
+++ src/components/App.js
@@ -13,23 +13,36 @@ const dataInput = data.map((data) => {
   let saleData = transactions.map((transactions) =>{
       points = calculatePoints(transactions.saleAmt);
       totalPoints += points;
-      return(<li className="list-group-item">Date: {transactions.saleDate} - Amount: {transactions.saleAmt} Transaction Points: {points}</li>);
+      return(<li className="list-group-item">Date: {transactions.saleDate} - Amount: ${transactions.saleAmt} - Points earned this transaction: {points}</li>);
   })
   return (
       <div className="card">
         <div className="card-header bg-info text-light">Customer: {data.customerName}</div>
         <div className="card-block">{saleData}</div>
         <div className="card-footer">
-            Points per month:
-            Total points:  {totalPoints}
+            <div>October points: {oneMonthTotal(transactions, "10")}</div>
+            <div>November points: {oneMonthTotal(transactions, "11")}</div>
+            <div>December points: {oneMonthTotal(transactions, "12")}</div>
+            <div>Total points:  {totalPoints}</div>
         </div>
       </div>
   );
 }
 )
 
-/*WIP*/
-function oneMonthTotal(points){
+/*
+* This collects points by month
+* 
+*/
+function oneMonthTotal(transactions, monthToFind){
+  var customerTransactions = JSON.parse(JSON.stringify(transactions));
+  var points = 0;
+  for(var i = 0; customerTransactions.length > i; i++){
+    let month = customerTransactions[i].saleDate.substring(0,2); 
+    if(month === monthToFind){
+      points += calculatePoints(customerTransactions[i].saleAmt);
+    }
+  }
   return points;
 }
 
@@ -51,9 +64,8 @@ function App() {
   return (
     <div className="App" >
       <h1>Reward Point Calculator</h1>
-      <div className="container-sm">
+      <div className="container-sm text-left col-md-6">
              {dataInput}
-            <button className="btn btn-primary">Clear</button>
           </div>
     </div>
   );
